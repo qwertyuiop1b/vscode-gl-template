74d5070e6301a8121f55eba79cf85371e5501e38
huanghuiqiang <1669903552@qq.com>
2022-12-01 20:24:43 +0800

    add texture partial

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <fstream>
 #include <sstream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -155,6 +158,15 @@ int main() {
 
     Shader ourShader("vert.glsl", "frag.glsl");
 
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+
 
     float vertices[] = {
         // 位置              // 颜色
@@ -162,6 +174,12 @@ int main() {
         -0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
         0.0f,  -0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
     }; 
+
+    float texCoords[] = {
+      0.0f, 0.0f,
+      1.0f, 0.0f,
+      0.5f, 0.5f,
+    };
 
     // // create VAO VBO
     unsigned int VAO, VBO;
